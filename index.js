ee1332a3379d91c08aae38d06505b1a1551ed6b5
poteto <naonyan1135@gmail.com>
2024-07-27 22:33:27 +0900

    comments

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 const express = require("express")
 const app = express()
 
-
-const verifyPassword = ("/secrets",(req,res,next)=>{ //* 関数式に変えちゃう
-  console.log(req.query)
-  const {password} = req.query
-  if(password === "supersecret") {
-   return  next() //* returnいる
-  }
-  // res.status(404).send("error")
-  throw new Error("パスワードが必要です") 
-})
-
+const verifyPassword =
+  ("/secrets",
+  (req, res, next) => {
+    //* 関数式に変えちゃう
+    console.log(req.query)
+    const { password } = req.query
+    if (password === "supersecret") {
+      return next() //* returnいる
+    }
+    // res.status(404).send("error")
+    throw new Error("パスワードが必要です")
+  })
 
 app.get("/", (req, res) => {
-  console.log(req.requestTime)
   res.send(`<h1>hello</h1>`)
 })
 
-
-app.get("/error",(req,res)=>{
+app.get("/error", (req, res) => {
   hoge.moge() //* わけわかんねえのだけやばれたらエラーがresponseで返commentsって行っちゃう
 })
 
-app.get("/secrets",verifyPassword,(req,res)=>{ //* next()がさすのがverifyPasswordの後ろだから前に書かなきゃダメ
+app.get("/secrets", verifyPassword, (req, res) => {
+  //* next()がさすのがverifyPasswordの後ろだから前に書かなきゃダメ
   res.send("<h1>this is a secret page!</h1>")
-},) //* コールバック関数無限
+}) //* コールバック関数無限
 
-app.use((req,res)=>{
+app.use((req, res) => {
   res.status(404).send("<h1>not found</h1>")
 })
 
-app.use((err,req,res,next)=>{ //* errorハンドラーは最後に書く
-console.log("***************************************")
-console.log("**************エラー!******************")
-console.log("***************************************")
+app.use((err, req, res, next) => {
+  //* errorハンドラーは最後に書く
+  //* http://localhost:3001/secrets?password=super とかにアクセスしたらパス自体はあるけどerrorが出る
+  //* それがデフォルトのエラーハンドラからこれになってる resは返らない
+  console.log("***************************************")
+  console.log("**************エラー!******************")
+  console.log("***************************************")
 })
-
 
 app.listen(3001, () => {
   console.log("localhost:3001で起動中")
